1e336a0fc05b62d3c1c5864600278ad502b2c346
Youssef-Salem-230104687 <youssef230104687@sut.edu.eg>
2025-02-22 23:40:14 +0200

    Update web.php

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -6,12 +6,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 1st way
 Route::get('/multable', function (Request $request) {
     $j = $request->number;
     $msg = $request->msg;
     // dd($j , $msg);
     return view('multable' , compact("j" , "msg"));
 });
+
+// 2nd way
+// Route::get('/multable/{number?}', function ($number = 9) {
+//     $j = $number;
+//     return view('multable' , compact("j"));
+// });
+
+// 3rd way
+// Route::get('/multable/{number?}', function ($number = null) {
+//         $j = $number??2;
+//         return view('multable' , compact("j"));
+//     });
 
 Route::get('/even', function () {
     return view('even');
